e6d0fb35b9a7a6b43c2a6a1edaaf6039a6f39f46
daashandayo <dj30c.1@gmail.com>
2019-04-29 20:02:36 +0530

    Add a command

--- commands.rb
+++ commands.rb
@@ -1,2 +1,6 @@
-$ require 'rest-client'
-$ RestClient.get 'www.bing.com'
+# require `rest-client` module
+require 'rest-client'
+# `GET` request to 'www.bing.com'
+RestClient.get 'www.bing.com'
+# `GET` request emulating search on 'www.bing.com'
+RestClient.get('https://www.bing.com/search', {params: {'q' => 'Endgame' }})
